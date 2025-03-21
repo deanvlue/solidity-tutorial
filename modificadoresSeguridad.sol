a6a279872379fc272f05754b63846d53ce67c4fe
deanvlue <munoz.josecarlos@gmail.com>
2025-03-21 17:16:45 -0600

    added receive function

--- modificadoresSeguridad.sol
+++ modificadoresSeguridad.sol
@@ -9,7 +9,7 @@ contract seguridadMod {
 
     modifier onlyOwner(){
         require(msg.sender==owner, "No tiene permiso para ejecutar esta funcion");
-        _;
+        _; // ahi pone el resto de la funcion
     }
 
     modifier hasEnoughFunds(uint256 _amount){
@@ -19,6 +19,26 @@ contract seguridadMod {
 
     constructor(){
         owner = msg.sender;
+    }
+
+    function depositar() public payable {
+        require(msg.value > 0, "Debe depositar una cantidad positiva");
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdrawal(uint256 _amount)  public hasEnoughFunds(_amount){
+        balances[msg.sender] -= _amount;
+        payable (msg.sender).transfer(_amount);
+    }
+
+    function retirarTodo() public onlyOwner{
+        uint256 balanceContrato = address(this).balance;
+        require(balanceContrato > 0, "no hay fondos en el contrato");
+        payable(owner).transfer(balanceContrato);
+    }
+
+    receive() external payable{
+        depositar();
     }
 
 }
